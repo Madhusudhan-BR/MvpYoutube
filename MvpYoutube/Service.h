567a81516ddfc044704193a5b4c29c07be7f6cae
Madhusudhan-BR <madhusudhanbram@gmail.com>
2017-06-19 07:27:38 +0530

    created singleton

---
+++ MvpYoutube/Service.h
@@ -0,0 +1,13 @@
+//
+//  Service.h
+//  MvpYoutube
+//
+//  Created by Madhusudhan B.R on 6/19/17.
+//  Copyright © 2017 Madhusudhan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Service : NSObject
++ (id) instance; 
+@end
---
+++ MvpYoutube/Service.m
@@ -0,0 +1,20 @@
+//
+//  Service.m
+//  MvpYoutube
+//
+//  Created by Madhusudhan B.R on 6/19/17.
+//  Copyright © 2017 Madhusudhan. All rights reserved.
+//
+
+#import "Service.h"
+
+@implementation Service
++ (id) instance {
+    
+
+
+    return self;
+}
+
+
+@end
